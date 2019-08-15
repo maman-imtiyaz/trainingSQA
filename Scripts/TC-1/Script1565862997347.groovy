125564d9c7da5ad14f0c16e19898da706fc77600
LENOVO <LENOVO@Rahman>
2019-08-15 16:59:59 +0700

    tambah script

--- Scripts/TC-1/Script1565862997347.groovy
+++ Scripts/TC-1/Script1565862997347.groovy
@@ -13,3 +13,5 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('http://203.190.54.74:86/jw/web/userview/oasysProjectManagement/oasysProjectManagement/_/welcome')
+
